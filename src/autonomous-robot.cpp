dcc42325d64d39ed774f53eeb656655affa1eff3
Adam Tonderski <atonderski@gmail.com>
2018-04-05 15:03:29 +0200

    Improved the main autonomous-robot program
    - added command line variables to control cid, speed, steering
        and run time
    - separated front/back and left/right inputs

--- src/autonomous-robot.cpp
+++ src/autonomous-robot.cpp
@@ -4,14 +4,15 @@
 #include "cluon-complete.hpp"
 #include "opendlv-standard-message-set.hpp"
 
-int32_t main(int32_t , char **) {
+int32_t main(int32_t argc, char **argv) {
     std::cout << "Hello! Activating super pro vehicle control " << std::endl;
-
+    auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
     float latestDistance;
     float latestVoltage;
-    cluon::OD4Session od4{111,
+    cluon::OD4Session od4{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"])),
         [&latestDistance, &latestVoltage](cluon::data::Envelope &&envelope) {
             // Here we should handle input data
+            uint32_t id = envelope.senderStamp();
             if (envelope.dataType() == 1090) {
                 opendlv::proxy::GroundSteeringRequest steeringMsg = cluon::extractMessage<opendlv::proxy::GroundSteeringRequest>(std::move(envelope));
                 std::cout << "Got ground steering request (probably from me): " << steeringMsg.groundSteering() << std::endl;
@@ -22,32 +23,55 @@ int32_t main(int32_t , char **) {
             }
             else if (envelope.dataType() == 1039) {
                 opendlv::proxy::DistanceReading distanceMsg = cluon::extractMessage<opendlv::proxy::DistanceReading>(std::move(envelope));
-                latestDistance = distanceMsg.distance();
-                std::cout << "Got distance message (from ultrasonic): " << distanceMsg.distance() << std::endl;
+                if (id == 1) {
+                    std::cout << "Got front distance message (from ultrasonic): " << distanceMsg.distance() << std::endl;
+                    latestDistance = distanceMsg.distance();
+                } else if (id == 2) {
+                    std::cout << "Got back distance message (from ultrasonic): " << distanceMsg.distance() << std::endl;
+                } else{
+                    std::cout << "Got unknown distance message (from ultrasonic): " << distanceMsg.distance() << std::endl;
+                }
             }
             else if (envelope.dataType() == 1037) {
                 opendlv::proxy::VoltageReading voltageMsg = cluon::extractMessage<opendlv::proxy::VoltageReading>(std::move(envelope));
-                latestVoltage = voltageMsg.voltage();
-                std::cout << "Got voltage message (from IR): " << voltageMsg.voltage() << std::endl;
+                if (id==1){
+                    std::cout << "Got left voltage message (from IR): " << voltageMsg.voltage() << std::endl;
+                    latestVoltage = voltageMsg.voltage();
+                } else if (id==2){
+                    std::cout << "Got right voltage message (from IR): " << voltageMsg.voltage() << std::endl;
+                } else{
+                    std::cout << "Got unknown voltage message (from IR): " << voltageMsg.voltage() << std::endl;
+                }
             }
             else {
                 std::cout << "Got some unknown data!" << std::endl;
             }
         }};
 
+    float pedalMagnitude = std::stof(commandlineArguments["pedal"]); 
+    float steeringMagnitude = std::stof(commandlineArguments["steering"]); 
+    float runTime = std::stof(commandlineArguments["time"]); 
+
+    float currentTime = 0;
     while (od4.isRunning()) {
-        std::this_thread::sleep_for(std::chrono::duration<double>(1.0));
+        std::this_thread::sleep_for(std::chrono::duration<double>(0.1));
+        currentTime += 0.1;
         // Here we should make decisions and send instructions
 
         opendlv::proxy::GroundSteeringRequest steeringMsg;
-        float nextSteering = latestDistance < 1 ? 1 : 0;
-        steeringMsg.groundSteering(nextSteering);
+        steeringMsg.groundSteering(steeringMagnitude);
         od4.send(steeringMsg);
 
         opendlv::proxy::PedalPositionRequest pedalPositionMsg;
-        float nextPosition = latestDistance < 1 ? 0.1 : 1;
-        pedalPositionMsg.position(nextPosition);
+        pedalPositionMsg.position(pedalMagnitude);
         od4.send(pedalPositionMsg);
+        if (currentTime > runTime){
+            pedalPositionMsg.position(0.0);
+            od4.send(pedalPositionMsg);
+            steeringMsg.groundSteering(0.0);
+            od4.send(steeringMsg);
+            break;
+        }
     }
 
     return 0;
