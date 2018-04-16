8d8acdb4292bc0d60f2990c65fc5f6531a1d5e94
Adam Tonderski <atonderski@gmail.com>
2018-04-16 14:49:30 +0200

    Up the frequency in docker-compose and slight logic change in simple controller

--- src/simplecontroller.cpp
+++ src/simplecontroller.cpp
@@ -11,23 +11,23 @@ void SimpleController::step(double) noexcept {
     double leftDistance = getLeftDistance();
     double rightDistance = getRightDistance();
 
-    double pedalPosition = 0.2;
+    double pedalPosition = 0.14;
     double groundSteeringAngle = 0.3;
     if (frontDistance < 0.3) {
         pedalPosition = 0.0;
     } else {
         if (rearDistance < 0.3) {
-            pedalPosition = 0.4;
+            pedalPosition = 0.0;
         }
     }
 
     if (leftDistance < rightDistance) {
         if (leftDistance < 0.2) {
-            groundSteeringAngle = 0.2;
+            groundSteeringAngle = -0.2;
         }
     } else {
         if (rightDistance < 0.2) {
-            groundSteeringAngle = -0.2;
+            groundSteeringAngle = 0.2;
         }
     }
 
