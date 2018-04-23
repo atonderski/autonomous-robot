8e96617a2cbf6b739ed7e3e5b4405eff537a5e13
adrian <adrianleidegren@mykolab.com>
2018-04-23 15:10:05 +0200

    Lots of changes

--- src/maintainDistance.cpp
+++ src/maintainDistance.cpp
@@ -1,28 +1,71 @@
 
 #include "maintainDistance.hpp"
 
-void MaintainDistance::step(double) noexcept
+UnlimitedStateWorks::UnlimitedStateWorks() noexcept:
+    m_frontDistance{std::numeric_limits<double>::max()},
+    m_rearDistance{std::numeric_limits<double>::max()},
+    m_leftDistance{std::numeric_limits<double>::max()},
+    m_rightDistance{std::numeric_limits<double>::max()},
+    m_groundSteeringAngle{0.0},
+    m_pedalPosition{0.0},
+    activeState{&UnlimitedStateWorks::initialState},
+    CRITICAL_DISTANCE{0.4},
+    MARGIN{0.1}
 {
-    double frontDistance = getFrontDistance();
-    //double rearDistance = getRearDistance();
-    //double leftDistance = getLeftDistance();
-    //double rightDistance = getRightDistance();
-
-    double groundSteeringAngle{0.0};
-    double speed;
-
-    double const CRITICAL_DISTANCE{0.4};
-    double const MARGIN{0.1};
-
-    if (frontDistance > (CRITICAL_DISTANCE + MARGIN)) {
-        speed = 0.16;
-    } else if (frontDistance < (CRITICAL_DISTANCE - MARGIN)) {
-        speed = -0.16;
-    } else {
-        speed = 0.0;
-    }
-
-    setGrounSteeringAngle(groundSteeringAngle);
-    setPedalPosition(speed);
-
 }
+
+void UnlimitedStateWorks::initialState() noexcept {
+    std::cout << "Now in Initial State" << std::endl;
+    activeState = &UnlimitedStateWorks::stateCorrectDistance;
+}
+
+void UnlimitedStateWorks::stateCloseDistance() noexcept {
+    m_groundSteeringAngle = 0.0;
+    m_pedalPosition = 0.7;
+    std::cout << "Now in Close Distance" << std::endl;
+    if (m_frontDistance < (CRITICAL_DISTANCE + MARGIN)) {
+        activeState = &UnlimitedStateWorks::stateCorrectDistance;
+    }
+}
+
+void UnlimitedStateWorks::stateMakeDistance() noexcept {
+    m_groundSteeringAngle = 0.0;
+    m_pedalPosition = -1.0;
+    std::cout << "Now in Make Distance" << std::endl;
+    if (m_frontDistance > (CRITICAL_DISTANCE - MARGIN)) {
+        activeState = &UnlimitedStateWorks::stateCorrectDistance;
+    }
+}
+
+void UnlimitedStateWorks::stateCorrectDistance() noexcept {
+    m_groundSteeringAngle = 0.0;
+    m_pedalPosition = 0.0;
+    std::cout << "Now in Correct Distance" << std::endl;
+    if (m_frontDistance < (CRITICAL_DISTANCE - MARGIN)) {
+        activeState = &UnlimitedStateWorks::stateMakeDistance;
+    } else if (m_frontDistance > (CRITICAL_DISTANCE + MARGIN)) {
+        activeState = &UnlimitedStateWorks::stateCloseDistance;
+    } else {
+        activeState = &UnlimitedStateWorks::stateCorrectDistance;
+    }
+}
+
+void UnlimitedStateWorks::update() noexcept
+{
+    if (activeState != 0) {
+        (this->*activeState)();
+    }
+}
+
+void MaintainDistance::step(double) noexcept {
+    m_satanicMill.m_frontDistance = getFrontDistance();
+    //m_satanicMill.m_rearDistance = getRearDistance();
+    //m_satanicMill.m_leftDistance = getLeftDistance();
+    //m_satanicMill.m_rightDistance = getRightDistance();
+
+    m_satanicMill.update();
+
+    setGrounSteeringAngle(m_satanicMill.m_groundSteeringAngle);
+    setPedalPosition(m_satanicMill.m_pedalPosition);
+}
+
--- src/maintainDistance.hpp
+++ src/maintainDistance.hpp
@@ -2,7 +2,38 @@
 #define AUTONOMOUS_ROBOT_MAINTAIN_DISTANCE
 
 #include "controller.hpp"
+#include <limits>
 #include <iostream>
+
+class UnlimitedStateWorks{
+    public:
+        void initialState() noexcept;
+        void update() noexcept;
+        
+        UnlimitedStateWorks() noexcept;
+        ~UnlimitedStateWorks() = default;
+
+        void stateCloseDistance() noexcept;
+        void stateMakeDistance() noexcept;
+        void stateCorrectDistance() noexcept;
+
+        double m_frontDistance;
+        double m_rearDistance;
+        double m_leftDistance;
+        double m_rightDistance;
+        double m_groundSteeringAngle;
+        double m_pedalPosition;
+    
+    private:
+        UnlimitedStateWorks(UnlimitedStateWorks const &) = delete;
+        UnlimitedStateWorks(UnlimitedStateWorks &&) = delete;
+        UnlimitedStateWorks &operator=(UnlimitedStateWorks const &) = delete;
+        UnlimitedStateWorks &operator=(UnlimitedStateWorks &&) = delete;
+        
+        void (UnlimitedStateWorks::* activeState)();
+        double const CRITICAL_DISTANCE;
+        double const MARGIN;
+};
 
 class MaintainDistance : public Controller {
 public:
@@ -13,7 +44,7 @@ public:
     void step(double) noexcept override;
 
 private:
-    
+    UnlimitedStateWorks m_satanicMill{};
 
 };
 
